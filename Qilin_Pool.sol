b761609bde25a8e446ef7adfbd9df2c3654b7489
Laubswind <martinzhao15@gmail.com>
2022-11-11 16:49:49 +0800

    minimize size

--- Qilin_Pool.sol
+++ Qilin_Pool.sol
@@ -39,8 +39,6 @@ contract Pool is QilinERC20{
     bool public paying_side_Last;                   //系统内记录上一次funding方向
     uint256 public funding_x_8h_upper;              //= 5000000000固定值，留接口修改
     uint256 public Base_rate;                       //= 156200，固定值，留接口修改
-    uint256 public N;
-    uint256 public N_Last;
     
     address public factory;
     address public tokenX;
@@ -124,11 +122,10 @@ contract Pool is QilinERC20{
 
     // 虚拟流动性更新
     function Virtual_Liquidity_Update() internal {
-        N = Math.cal_N(True_Liquid_X , B , Peqx , True_Liquid_Y , C , Peqy , True_Liquid_X0 , True_Liquid_Y0); 
+        uint N = Math.cal_N(True_Liquid_X , B , Peqx , True_Liquid_Y , C , Peqy , True_Liquid_X0 , True_Liquid_Y0); 
         D = D0 * N / const18;
         Xeq = D/2;
         Yeq = Xeq;
-        N_Last = N;
     }
 
     // 添加流动性
@@ -159,235 +156,156 @@ contract Pool is QilinERC20{
     }
 
     //现货/合约 交易，用X买Y input为 delta：用户支付的X值，perp：是否为合约交易，output为 Yget：用户支付的X值所能换出的Y值
-    function Trade_XtoY(uint deltaX, bool perp) internal lock returns(uint Yget) {
+    function Trade_XtoY(uint deltaX, bool perp, uint exactY) internal lock returns(uint Yget) {
 
-        bool check = true;
-        while(check == true){
+        uint Xin;
+        while(deltaX - Xin > Math.cal_cross(Math.cal_times(Xeq , Tick_range , const18), X_Coord, Peqx)){
             //计算此时曲线内触发跨tick的X值
             uint X_Coord_Test = Math.cal_times(Xeq , Tick_range , const18);
 
             //计算对应的Y值
             uint Y_Coord_Test = Math.cal_Y(B, C, D, X_Coord_Test);
 
-            //检测当前输入deltaX是否大于跨tick所需X
-            if(deltaX > Math.cal_cross(X_Coord_Test, X_Coord, Peqx)){
+
+            if( exactY > 0 && Yget + Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy) > exactY) break;
+
+            Yget += Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy);
+
+            //deltaX减少跨tick所用的这部分X
+            Xin += Math.cal_cross(X_Coord_Test, X_Coord, Peqx);
+
+            //如果是perp交易，则累加系统内总的Y Position
+            if(perp == true) {
+                Ygetp += Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy);
                 
-                //计算累加跨tick前所需X能换得的Y
-                Yget += Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy);
-
-                //deltaX减少跨tick所用的这部分X
-                deltaX -= Math.cal_cross(X_Coord_Test, X_Coord, Peqx);
-
-                //如果是perp交易，则累加系统内总的Y Position
-                if(perp == true) {
-                    Ygetp += Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy);
-                
-                //如果是现货 按这一小笔交易（即跨tick所需X -> Y）的进出更新 X和Y的实际资产存量
-                }else{
-                    True_Liquid_X += Math.cal_cross(X_Coord_Test, X_Coord, Peqx);
-                    True_Liquid_Y -= Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy);
-                }
-
-                // 按照跨tick后系统状态更新Peqx Peqy
-                Peqy = Math.cal_times(Peqy , Yeq , Y_Coord_Test);
-                Peqx = Math.cal_times(Peqx , Xeq , X_Coord_Test);
-
-                //计算跨tick后的曲线斜率所对应的Price_local
-                Price_local = Math.cal_times2 (Math.cal_price(B, C, D, X_Coord_Test, Y_Coord_Test) , Y_Coord_Test , Xeq , X_Coord_Test , Yeq );
-                
-                //更新 跨tick后X Y坐标
-                //计算此时对应的B C值
-                B = Math.cal_B(Xeq, Yeq, D, Price_local);
-                C = 2 * const18 - B; 
-
-                //根据新的各参数 更正流动性
-                Virtual_Liquidity_Update();
-
-                //将更新流动性后的参数传入曲线
-                X_Coord = Xeq;
-                Y_Coord = Yeq;
-
+            //如果是现货 按这一小笔交易（即跨tick所需X -> Y）的进出更新 X和Y的实际资产存量
             }else{
-                //记录此时的X Y坐标
-                
-                uint Y_Coord_Last = Y_Coord;
-
-                //按剩余deltaX 更新X Y坐标
-                X_Coord +=  Math.cal_times(deltaX , Peqx , const18);
-                Y_Coord = Math.cal_Y(B, C, D, X_Coord);
-
-                //计算累加这部分X能换得的Y
-                Yget += Math.cal_cross(Y_Coord_Last, Y_Coord, Peqy);
-
-                //如果是perp交易，则累加系统内总的Y Position
-                if(perp == true){
-                    Ygetp += Math.cal_cross(Y_Coord_Last, Y_Coord, Peqy);
-
-                //如果是现货 按这一小笔交易的进出更新 X和Y的实际资产存量
-                }else{
-                    True_Liquid_X += deltaX;
-                    True_Liquid_Y -= Math.cal_cross(Y_Coord_Last, Y_Coord, Peqy);
-                }
-                check = false;
+                True_Liquid_X += Math.cal_cross(X_Coord_Test, X_Coord, Peqx);
+                True_Liquid_Y -= Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy);
             }
-        }
-    }
 
-    //现货/合约 交易，用X买Y，input为 delta：所需的目标Y值， Xmax：用户可以支付的X值上限，perp：是否是合约交易，output为Xin：为换出所需Y值，用户所要支付的精确X值
-    function TradeXToExactY(uint deltaY, uint Xmax, bool perp) internal lock returns(uint Xin) {
- 
-        bool check = true;
-        while(check == true){
+            // 按照跨tick后系统状态更新Peqx Peqy
+            Peqy = Math.cal_times(Peqy , Yeq , Y_Coord_Test);
+            Peqx = Math.cal_times(Peqx , Xeq , X_Coord_Test);
+
+            //计算跨tick后的曲线斜率所对应的Price_local
+            Price_local = Math.cal_times2 (Math.cal_price(B, C, D, X_Coord_Test, Y_Coord_Test) , Y_Coord_Test , Xeq , X_Coord_Test , Yeq );
             
-            //计算此时曲线内触发跨tick的X值
-            uint X_Coord_Test = Math.cal_times(Xeq , Tick_range , const18);
+            //更新 跨tick后X Y坐标
+            //计算此时对应的B C值
+            B = Math.cal_B(Xeq, Yeq, D, Price_local);
+            C = 2 * const18 - B; 
 
-            //计算对应的Y值
-            uint Y_Coord_Test = Math.cal_Y(B, C, D, X_Coord_Test);
+            //根据新的各参数 更正流动性
+            Virtual_Liquidity_Update();
 
-            //检测所需Y值是否大于跨tick会换出的Y值
-            if(deltaY > Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy)){
+            //将更新流动性后的参数传入曲线
+            X_Coord = Xeq;
+            Y_Coord = Yeq;
 
-                //累加所需精确X输入值
-                Xin += Math.cal_cross(X_Coord_Test, X_Coord, Peqx);
-
-                //减少对应的目标Y值
-                deltaY -= Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy);
-
-                //检测是否为合约交易，是的话更改总position参数，不是的话更改真实流动性参数
-                if(perp == true){
-                    Ygetp += Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy);
-                }else{
-                    True_Liquid_X +=  Math.cal_cross(X_Coord_Test, X_Coord, Peqx);
-                    True_Liquid_Y -=  Math.cal_cross(Y_Coord, Y_Coord_Test, Peqy);
-                }
-
-
-                // 按照跨tick后系统状态更新Peqx Peqy  计算跨tick后的曲线斜率所对应的Price_local
-                Peqy = Math.cal_times(Peqy , Yeq , Y_Coord_Test);
-                Peqx = Math.cal_times(Peqx , Xeq , X_Coord_Test);
-                Price_local = Math.cal_times2 (Math.cal_price(B, C, D, X_Coord_Test, Y_Coord_Test) , Y_Coord_Test , Xeq , X_Coord_Test , Yeq );
-                
-                B = Math.cal_B(Xeq, Yeq, D, Price_local);
-                C = 2 * const18 - B;
-                Virtual_Liquidity_Update();
-                X_Coord = Xeq;
-                Y_Coord = Yeq;
-
-            }else{
-                uint X_Coord_Last = X_Coord;
-                
-                Y_Coord -= Math.cal_times(deltaY , Peqy , const18);
-                X_Coord = Math.cal_X(B, C, D, Y_Coord);
-                Xin += Math.cal_cross(X_Coord, X_Coord_Last, Peqx);
-                if(perp == true){
-                    Ygetp += deltaY;
-                }else{
-                    True_Liquid_X += Math.cal_cross(X_Coord, X_Coord_Last, Peqx);
-                    True_Liquid_Y -= deltaY;
-                }
-                check = false;
-            }
         }
-        //对于 现货交易，需检测 计算出的所需X输入值是否小于用户能支付的X值上限
-        if(perp == false) require(Xmax > Xin);
+        
+
+        if (exactY == 0){
+            //记录此时的X Y坐标
+                
+            uint Y_Coord_Last = Y_Coord;
+
+            //按剩余deltaX 更新X Y坐标
+            X_Coord +=  Math.cal_times(deltaX - Xin, Peqx , const18);
+            Y_Coord = Math.cal_Y(B, C, D, X_Coord);
+
+            //计算累加这部分X能换得的Y
+            Yget += Math.cal_cross(Y_Coord_Last, Y_Coord, Peqy);
+
+            //如果是perp交易，则累加系统内总的Y Position
+            if(perp == true){
+                Ygetp += Math.cal_cross(Y_Coord_Last, Y_Coord, Peqy);
+
+            //如果是现货 按这一小笔交易的进出更新 X和Y的实际资产存量
+            }else{
+                True_Liquid_X += deltaX - Xin;
+                True_Liquid_Y -= Math.cal_cross(Y_Coord_Last, Y_Coord, Peqy);
+            }
+        }else{
+            uint X_Coord_Last = X_Coord;
+                
+            Y_Coord -= Math.cal_times(exactY - Yget, Peqy , const18);
+            X_Coord = Math.cal_X(B, C, D, Y_Coord);
+            Xin += Math.cal_cross(X_Coord, X_Coord_Last, Peqx);
+            if(perp == true){
+                Ygetp += exactY - Yget;
+            }else{
+                True_Liquid_X += Math.cal_cross(X_Coord, X_Coord_Last, Peqx);
+                True_Liquid_Y -= exactY - Yget;
+            }
+            Yget = Xin;
+        }   
+            
+        
     }
 
     //现货/合约  交易，用Y买X input为Y
-    function Trade_YtoX(uint deltaY , bool perp) internal lock returns(uint Xget) {
+    function Trade_YtoX(uint deltaY , bool perp , uint exactX) internal lock returns(uint Xget) {
 
-        bool check = true;
-        while(check == true){
+        uint Yin;
+        while(deltaY - Yin > Math.cal_cross(Math.cal_times(Yeq , Tick_range , const18), Y_Coord, Peqy)){
 
             uint Y_Coord_Test = Math.cal_times(Yeq , Tick_range , const18);
             uint X_Coord_Test = Math.cal_X(B, C, D, Y_Coord_Test);
 
-            if(deltaY > Math.cal_cross(Y_Coord_Test, Y_Coord, Peqy)){
+            if(exactX > 0 && Xget + Math.cal_cross(X_Coord, X_Coord_Test, Peqx) > exactX ) break;
 
-                Xget += Math.cal_cross(X_Coord, X_Coord_Test, Peqx);
-                deltaY -= Math.cal_cross(Y_Coord_Test, Y_Coord, Peqy);
-                if(perp == true){
-                    Xgetp += Math.cal_cross(X_Coord, X_Coord_Test, Peqx);
-                }else{
-                    True_Liquid_X -= Math.cal_cross(X_Coord, X_Coord_Test, Peqx);
-                    True_Liquid_Y += Math.cal_cross(Y_Coord_Test, Y_Coord, Peqy);
-                }
+            Xget += Math.cal_cross(X_Coord, X_Coord_Test, Peqx);
+            Yin += Math.cal_cross(Y_Coord_Test, Y_Coord, Peqy);
 
-                Peqy = Math.cal_times(Peqy , Yeq , Y_Coord_Test);
-                Peqx = Math.cal_times(Peqx , Xeq , X_Coord_Test);
-                Price_local = Math.cal_times2 (Math.cal_price(B, C, D, X_Coord_Test, Y_Coord_Test) , Y_Coord_Test , Xeq , X_Coord_Test , Yeq );
+            if(perp == true){
+                Xgetp += Math.cal_cross(X_Coord, X_Coord_Test, Peqx);
+            }else{
+                True_Liquid_X -= Math.cal_cross(X_Coord, X_Coord_Test, Peqx);
+                True_Liquid_Y += Math.cal_cross(Y_Coord_Test, Y_Coord, Peqy);
+            }
+
+            Peqy = Math.cal_times(Peqy , Yeq , Y_Coord_Test);
+            Peqx = Math.cal_times(Peqx , Xeq , X_Coord_Test);
+            Price_local = Math.cal_times2 (Math.cal_price(B, C, D, X_Coord_Test, Y_Coord_Test) , Y_Coord_Test , Xeq , X_Coord_Test , Yeq );
                 
 
-                B = Math.cal_B(Xeq, Yeq, D, Price_local);
-                C = 2 * const18 - B;
-                Virtual_Liquidity_Update();
-                X_Coord = Xeq;
-                Y_Coord = Yeq;
+            B = Math.cal_B(Xeq, Yeq, D, Price_local);
+            C = 2 * const18 - B;
+            Virtual_Liquidity_Update();
+            X_Coord = Xeq;
+            Y_Coord = Yeq;
 
-            }else{
+            }
+
+
+            if(exactX == 0){
                 uint X_Coord_Last = X_Coord;
                 
-                Y_Coord += Math.cal_times(deltaY , Peqy , const18);
+                Y_Coord += Math.cal_times(deltaY - Yin, Peqy , const18);
                 X_Coord = Math.cal_X(B, C, D, Y_Coord);
                 Xget += Math.cal_cross(X_Coord_Last, X_Coord, Peqx);
                 if(perp == true){
                     Xgetp += Math.cal_cross(X_Coord_Last, X_Coord, Peqx);
                 }else{
-                    True_Liquid_Y += deltaY;
+                    True_Liquid_Y += deltaY - Yin;
                     True_Liquid_X -= Math.cal_cross(X_Coord_Last, X_Coord, Peqx);
                 }
-                check = false;
-            }
-        }
-    }
-
-    //现货/合约  交易，用Y买X input为X
-    function TradeYToExactX(uint deltaX, uint Ymax, bool perp) internal lock returns(uint Yin) {
-
-        bool check = true;
-        while(check == true){
-
-            uint Y_Coord_Test = Math.cal_times(Yeq , Tick_range , const18);
-            uint X_Coord_Test = Math.cal_X(B, C, D, Y_Coord_Test);
-
-            if(deltaX > Math.cal_cross(X_Coord, X_Coord_Test, Peqx)){
-
-                Yin += Math.cal_cross(Y_Coord_Test, Y_Coord, Peqy);
-                deltaX -= Math.cal_cross(X_Coord, X_Coord_Test, Peqx);
-                if(perp == true){
-                    Xgetp += Math.cal_cross(X_Coord, X_Coord_Test, Peqx);
-                }else{
-                    True_Liquid_Y +=  Math.cal_cross(Y_Coord_Test, Y_Coord, Peqy);
-                    True_Liquid_X -=  Math.cal_cross(X_Coord, X_Coord_Test, Peqx);
-                }
-
-                Peqy = Math.cal_times(Peqy , Yeq , Y_Coord_Test);
-                Peqx = Math.cal_times(Peqx , Xeq , X_Coord_Test);
-                Price_local = Math.cal_times2 (Math.cal_price(B, C, D, X_Coord_Test, Y_Coord_Test) , Y_Coord_Test , Xeq , X_Coord_Test , Yeq );
-                
-
-                B = Math.cal_B(Xeq, Yeq, D, Price_local);
-                C = 2 * const18 - B;
-                Virtual_Liquidity_Update();
-                X_Coord = Xeq;
-                Y_Coord = Yeq;
-
             }else{
-                
                 uint Y_Coord_Last = Y_Coord;
-                X_Coord -= Math.cal_times(deltaX , Peqx , const18);
+                X_Coord -= Math.cal_times(exactX - Xget , Peqx , const18);
                 Y_Coord = Math.cal_Y(B, C, D, X_Coord);
                 Yin += Math.cal_cross(Y_Coord, Y_Coord_Last, Peqy);
                 if(perp == true){
-                    Xgetp += deltaX;
+                    Xgetp += exactX - Xget;
                 }else{ 
                     True_Liquid_Y += Math.cal_cross(Y_Coord, Y_Coord_Last, Peqy);
-                    True_Liquid_X -= deltaX;
+                    True_Liquid_X -= exactX - Xget;
                 }
-                check = false;
+                Xget = Yin;
             }
-        }
-        if(perp == false) require(Ymax > Yin);
+        
     }
 
     // 总的swap function 现货交易时调用  input为 to：资产输出地址 ，Xout：用户所需Y to exact X 的精准X输出值， Yout：用户所需X to exact Y 的精准X输出值；如果Xout&Yout均为0，则代表用户无需exact，只需将输入资产全部输出即可
@@ -488,11 +406,11 @@ contract Pool is QilinERC20{
         uint256 In;
         if(XtoY == true){
             if(delta_X > 0){
-                Out = Trade_XtoY(delta_X , true);
+                Out = Trade_XtoY(delta_X , true , 0);
                 In = delta_X;
             }else{
                 Out = delta_Y;
-                In = TradeXToExactY(delta_Y , 0 , true);
+                In = Trade_XtoY(0 , true , delta_Y );//
             }
             debt_index[userID][false].debttoken_amount += Math.cal_times(In , Total_debttoken_X , Total_debt_X);
             debt_index[userID][true].position_amount += Out;
@@ -501,9 +419,9 @@ contract Pool is QilinERC20{
         }else{
             if(delta_X > 0){
                 Out = delta_X;
-                In = TradeYToExactX(delta_X , 0 , true);
+                In = Trade_YtoX( 0 , true , delta_X );//
             }else{
-                Out = Trade_YtoX(delta_Y , true);
+                Out = Trade_YtoX(delta_Y , true , 0);
                 In = delta_Y;
             }
             debt_index[userID][true].debttoken_amount += Math.cal_times(In , Total_debttoken_Y , Total_debt_Y);
@@ -531,7 +449,7 @@ contract Pool is QilinERC20{
         require(delta_X * delta_Y == 0 && delta_X + delta_Y > 0 && Net_Margin(userID) + Net_Position(userID) > Net_debt(userID) && delta_X <= debt_index[userID][false].position_amount && delta_Y <= debt_index[userID][true].position_amount);
         refresh_totalbook();
         if(delta_X > 0){   
-            uint Get = Trade_XtoY(delta_X , true);
+            uint Get = Trade_XtoY(delta_X , true , 0);
             uint Y_close = Math.cal_times2(delta_X , debt_index[userID][true].debttoken_amount , Total_debt_Y , Total_debttoken_Y , debt_index[userID][false].position_amount) ;
             debt_index[userID][false].position_amount -= delta_X;
             Total_debt_Y -= Y_close ; 
@@ -539,15 +457,15 @@ contract Pool is QilinERC20{
             debt_index[userID][true].debttoken_amount -= Math.cal_times(debt_index[userID][true].debttoken_amount , delta_X , debt_index[userID][false].position_amount);
 
             if(Get > Y_close && Two_white == false){
-                margin_index[userID][0] += Trade_YtoX(Get - Y_close , false);
+                margin_index[userID][0] += Trade_YtoX(Get - Y_close , false , 0);
             }else if(margin_index[userID][1] + Get > Y_close && Two_white == true){
                 margin_index[userID][1] = margin_index[userID][1] + Get - Y_close;
             }else{
-                margin_index[userID][0] = margin_index[userID][0] - TradeXToExactY(Y_close - Get - margin_index[userID][1] , margin_index[userID][0], false);
+                margin_index[userID][0] = margin_index[userID][0] - Trade_XtoY( margin_index[userID][0], false , Y_close - Get - margin_index[userID][1]);
                 margin_index[userID][1] = 0;
             }
         }else{
-            uint Get = Trade_YtoX(delta_Y , true);
+            uint Get = Trade_YtoX(delta_Y , true , 0);
             uint X_close = Math.cal_times2(delta_Y , debt_index[userID][false].debttoken_amount , Total_debt_X , Total_debttoken_X , debt_index[userID][true].position_amount) ;
             debt_index[userID][true].position_amount -= delta_Y;
             Total_debt_X -= X_close ; 
@@ -557,7 +475,7 @@ contract Pool is QilinERC20{
             if(margin_index[userID][0] + Get > X_close){
                 margin_index[userID][0] = margin_index[userID][0] + Get - X_close;
             }else{
-                margin_index[userID][1] = margin_index[userID][1] - TradeYToExactX(X_close - Get - margin_index[userID][0], margin_index[userID][1], false);
+                margin_index[userID][1] = margin_index[userID][1] - Trade_YtoX( margin_index[userID][1], false , X_close - Get - margin_index[userID][0]);
                 margin_index[userID][0] = 0;
             }
         }
